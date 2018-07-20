6b52ff9bc87d714cb13d990c5358feee75f0fe6a
Gabriele Venturato <venturato.gabriele@gmail.com>
2018-07-20 23:39:01 +0200

    fixed some details in benchmark script

--- asp/benchmark.sh
+++ asp/benchmark.sh
@@ -3,7 +3,9 @@
 # Automated Reasoning 2017/2018
 # Script to run the asp domino model on all instances in "input/" folder.
 
-POSITIONAL=()
+# launch "./benchmark.sh" to test on all the instances
+# launch "./benchmark.sh -s" (or --sample) to test on 10% of all instances
+
 SAMPLE=false
 TOT=100
 while [[ $# -gt 0 ]]
@@ -29,7 +31,17 @@ fi
 samplingposition=$((RANDOM % 10)) # from 0 to 9
 
 ## clean output directory
-rm -f output/*.txt
+# if sampling erase and do a new sampling
+# if not sampling and output files already exists: warning and stop
+if [ "$SAMPLE" = false ]; then
+    if [ "$(ls -A output | grep txt)" ]; then
+        echo "WATCH OUT! You have already generate some outputs."
+        echo "Check what's going on. If you are not interested anymore: clean!"
+        exit 1
+    fi
+else
+    rm -f output/sample/*.txt
+fi
 
 ## loop through all instances
 index=0
@@ -39,6 +51,9 @@ for filename in input/*; do
     f=$(basename $filename)
     f=${f%.*}
     outfile="output/out_"${f##in_}".txt"
+    if [ "$SAMPLE" = true ]; then
+        outfile="output/sample/out_"${f##in_}".txt"
+    fi
     # execute if sampling is not sette up
     # OR if it is setted up and this is a samplingposition
     if [[ "$SAMPLE" = false ]] || 
